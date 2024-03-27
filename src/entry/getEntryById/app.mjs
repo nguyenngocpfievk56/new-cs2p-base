311371c00e45fc3352b324f35fee2809b89bc1d7
nguyenngocpfievk56 <nguyen.huu.ngoc@elife.co.jp>
2024-03-27 23:51:53 +0700

    update

--- src/entry/getEntryById/app.mjs
+++ src/entry/getEntryById/app.mjs
@@ -2,7 +2,7 @@ export const lambdaHandler = async (event, _) => {
     const data = {
         id: 1,
         title: 'Base 1',
-        content: 'This is the first Base entry'
+        content: 'This is the first Base entry',
     };
 
     const response = {
